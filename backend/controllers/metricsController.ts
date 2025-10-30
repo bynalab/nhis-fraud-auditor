0d8a5d3f5dd3d1992b488e2ef92ba95e13f8a872
bynalab <bynalabs@gmail.com>
2025-10-30 12:20:20 +0100

    Refactor getMetrics function in metricsController to optimize database queries by using Promise.all for concurrent execution, improving performance and reducing response time.

--- backend/controllers/metricsController.ts
+++ backend/controllers/metricsController.ts
@@ -5,27 +5,26 @@ import { FRAUD_HIGH_THRESHOLD } from "../utils/constant";
 
 export async function getMetrics(req: Request, res: Response) {
   try {
-    const total = await Claim.count();
-
-    const avgResult = (await Claim.findOne({
-      attributes: [
-        [
-          Claim.sequelize!.fn("AVG", Claim.sequelize!.col("claim_charge")),
-          "avg_charge",
+    const [avgResult, flagged, total, low, medium, high] = await Promise.all([
+      (await Claim.findOne({
+        attributes: [
+          [
+            Claim.sequelize!.fn("AVG", Claim.sequelize!.col("claim_charge")),
+            "avg_charge",
+          ],
         ],
-      ],
-      raw: true,
-    })) as any;
-
-    const flagged = await Claim.count({
-      where: {
-        fraud_score: { [Op.gt]: FRAUD_HIGH_THRESHOLD },
-      },
-    });
-
-    const low = await Claim.count({ where: { fraud_category: "Low" } });
-    const medium = await Claim.count({ where: { fraud_category: "Medium" } });
-    const high = await Claim.count({ where: { fraud_category: "High" } });
+        raw: true,
+      })) as any,
+      await Claim.count({
+        where: {
+          fraud_score: { [Op.gt]: FRAUD_HIGH_THRESHOLD },
+        },
+      }),
+      await Claim.count(),
+      Claim.count({ where: { fraud_category: "Low" } }),
+      Claim.count({ where: { fraud_category: "Medium" } }),
+      Claim.count({ where: { fraud_category: "High" } }),
+    ]);
 
     const totalClaims = total || 0;
     const flaggedCount = flagged || 0;
